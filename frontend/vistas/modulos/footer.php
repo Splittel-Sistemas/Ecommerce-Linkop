5cdab25d2788016df35d37ca53ec192711a7c8e8
RMN <50149082+Ramon-Olea@users.noreply.github.com>
2022-06-27 12:29:18 -0500

    cambios footer

--- frontend/vistas/modulos/footer.php
+++ frontend/vistas/modulos/footer.php
@@ -8,22 +8,15 @@ FOOTER
 		<div class="row">
 			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 formContacto  text-center">
 
-				<h4 style="color: white;">Sé el primero en conocer nuestros nuevos productos</h4>
-				<p style="color: white;">Regístrate a nuestra newsletter de clientes preferenciales y recibe promociones personalizadas.</p>
-				<form role="form" method="post" onsubmit="return validarContactenos()">
+				<form class="form-inline" method="post" onsubmit="return validarContactenos()">
+				<label for="exampleInputEmail2">
+							<h4 style="color: white;">STAY CONNECTD</h4>
+						</label>
+					<div class="form-group">
+					
+						<input type="email" id="emailContactenos" name="emailContactenos" class="form-control" placeholder="Escriba su correo electrónico" required>
 
-					<!-- <input type="text" id="nombreContactenos" name="nombreContactenos" class="form-control" placeholder="Escriba su nombre" required> 
-
-							<br> -->
-
-					<input type="email" id="emailContactenos" name="emailContactenos" class="	form-control" placeholder="Escriba su correo electrónico" required>
-
-					<!--    	          
-						<textarea id="mensajeContactenos" name="mensajeContactenos" class="form-control" placeholder="Escriba su mensaje" rows="5" required></textarea>
-
-						<br> -->
-						<br>
-
+					</div>
 					<input type="submit" value="Subscribe" class="btn btn-default backColor pull-center" id="enviar">
 
 				</form>
@@ -34,42 +27,40 @@ FOOTER
 				$contactenos->ctrFormularioContactenos();
 
 				?>
-
 			</div>
 
-			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 text-center social">
-						<br>
-						<br>
-						<br>
 
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 formContacto  text-center">
 				<ul>
-				<li>			<!-- <li>
+					<li>
+						<!-- <li>
 							<a target="_blank" href="https://api.whatsapp.com/send?phone=+526692710170&amp;text=%C2%A1Hola! Me gustario obtener informacion sobre Linkop">
 								<img src="<?php #echo $url; 
 											?>vistas/img/whatsapp(2).png" style="padding-bottom: 5px;"></a>
 						</li> -->
-					<?php
+						<?php
 
-					$social = ControladorPlantilla::ctrEstiloPlantilla();
+						$social = ControladorPlantilla::ctrEstiloPlantilla();
 
-					$jsonRedesSociales = json_decode($social["redesSociales"], true);
+						$jsonRedesSociales = json_decode($social["redesSociales"], true);
 
-					foreach ($jsonRedesSociales as $key => $value) {
+						foreach ($jsonRedesSociales as $key => $value) {
 
-						if ($value["activo"] == 1) {
+							if ($value["activo"] == 1) {
 
-										echo '
-						<a href="' . $value["url"] . '" target="_blank">
-							<i class="fa ' . $value["red"] . ' redSocial ' . $value["estilo"] . '" aria-hidden="true"></i>
+								echo '
+						<a href="' . $value["url"] . '" target="_blank" ">
+							<i class="fa ' . $value["red"] . ' redSocial ' . $value["estilo"] . '  fa-2x" aria-hidden="true"> </i>
 						</a>
+						&nbsp
 					';
-									}
-								}
+							}
+						}
 
-					?>
+						?>
 
 
-						</li>
+					</li>
 
 				</ul>
 
@@ -80,37 +71,48 @@ FOOTER
 			<!--=====================================
 			CATEGORÍAS Y SUBCATEGORÍAS FOOTER
 			======================================-->
+			<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12  text-center ">
 
-			<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12  text-center">
-
-				<h4><a href="#" class="" titulo="CONOCENOS" style="color: white;">QUIENES SOMOS</a></h4>
-
+				<h4><a href="#" class="" titulo="BLOG" style="color: white;">INFO</a></h4>
 				<ul>
-					<li><a href="#" class="pixelCategorias"> Nuestra historia</a></li>
-				</ul>
-				<ul>
-					<li><a href="#" class="pixelCategorias">Términos y Condiciones</a></li>
-				</ul>
+					<li><a href="#" class="pixelCategorias" style="color: gray;">Our Story</a></li>
 
-				<ul>
-					<li><a href="#" class="pixelCategorias">Política de Privacidad</a></li>
-				</ul>
+					<li><a href="#" class="pixelCategorias" style="color: gray;">Careers</a></li>
 
-				<ul>
-					<li><a href="#" class="pixelCategorias">Política de devoluciones</a></li>
-				</ul>
+					<li><a href="#" class="pixelCategorias" style="color: gray;">Bulk Order Inquiry</a></li>
+					<li><a href="#" class="pixelCategorias" style="color: gray;">Customer Reviews</a></li>
 
+
+
+				</ul>
 
 
 			</div>
-			<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12  text-center">
+			<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12  text-center ">
+
+				<h4><a href="#" class="" titulo="BLOG" style="color: white;">LEGAL</a></h4>
+				<ul>
+					<li><a href="#" class="pixelCategorias" style="color: gray;">Privacy</a></li>
+
+					<li><a href="#" class="pixelCategorias" style="color: gray;">Terms</a></li>
+
+					<li><a href="#" class="pixelCategorias" style="color: gray;">Patents</a></li>
+					<li><a href="#" class="pixelCategorias" style="color: gray;">Counterfeits</a></li>
+
+
+
+				</ul>
+
+
+			</div>
+			<!-- <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12  text-center">
 
 				<h4><a href="#" class="" titulo="PRODUCTOS" style="color: white;">PRODUCTOS</a></h4>
 
 
 
 				<?php
-
+				/* 
 				$url = Ruta::ctrRuta();
 
 				$item = null;
@@ -122,42 +124,42 @@ FOOTER
 
 
 					echo '<ul> <li><a href="' . $url . $value["ruta"] . '" class="pixelCategorias" titulo="' . $value["categoria"] . '">' . $value["categoria"] . '</a></li> </ul>';
-				}
+				} */
 
 				?>
+			</div> -->
+			<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12  text-center ">
+
+				<h4><a href="#" class="" titulo="BLOG" style="color: white;">SUPPORT</a></h4>
+				<ul>
+					<li><a href="#" class="pixelCategorias" style="color: gray;">Contact</a></li>
+
+					<li><a href="#" class="pixelCategorias" style="color: gray;">Shipping</a></li>
+
+					<li><a href="#" class="pixelCategorias" style="color: gray;">Return</a></li>
+					<li><a href="#" class="pixelCategorias" style="color: gray;">Warranty</a></li>
+
+					<li><a href="#" class="pixelCategorias" style="color: gray;">Reward Policy</a></li>
+					<li><a href="#" class="pixelCategorias" style="color: gray;">Web Accessibility</a></li>
+					<li><a href="#" class="pixelCategorias" style="color: gray;">FAQ</a></li>
+
+				</ul>
+
+
 			</div>
 			<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12  text-center ">
 
-				<h4><a href="#" class="" titulo="BLOG" style="color: white;">BLOG</a></h4>
+				<h4><a href="#" class="" titulo="BLOG" style="color: white;">OTHERS</a></h4>
 				<ul>
-					<li><a href="#" class="pixelCategorias"> Nuestra historia</a></li>
+					<li><a href="#" class="pixelCategorias" style="color: gray;">My Account</a></li>
+
+					<li><a href="#" class="pixelCategorias" style="color: gray;">Reward Program</a></li>
+
+					<li><a href="#" class="pixelCategorias" style="color: gray;">Tracking Order</a></li>
+
+
 				</ul>
-				<ul>
-					<li><a href="#" class="pixelCategorias">Términos y Condiciones</a></li>
-				</ul>
 
-				<ul>
-					<li><a href="#" class="pixelCategorias">Política de Privacidad</a></li>
-				</ul>
-
-
-			</div>
-			<div class="col-md-3 col-sm-12 col-xs-12 text-left infoContacto  text-center">
-
-				<h5 style="color: white;">CONTACTO</h5>
-
-
-				<h5>
-
-					<i class="fa fa-phone-square" aria-hidden="true" style="color: white;"></i> Llámanos: 800 134 26 90
-
-					<br><br>
-
-					<i class="fa fa-envelope" aria-hidden="true" style="color: white;"></i> ventas@fibremex.com.mx
-
-
-
-				</h5>
 
 			</div>
 			<!--=====================================
@@ -174,11 +176,60 @@ FOOTER
 
 		</div>
 
+		<div class="container-fluid final">
+
+			<div class="">
+
+				<div class="row">
+
+					<div class="col-sm-12 col-xs-12 text-left text-muted">
+
+						<h5>&copy; Todos los derechos reservados. Hecho por Grupo Splittel</h5>
+
+					</div>
+
+					<!-- 	<div class="col-sm-6 col-xs-12 text-right social">
+
+						<ul>
+							<li>
+								<a target="_blank" href="https://api.whatsapp.com/send?phone=+526692710170&amp;text=%C2%A1Hola! Me gustario obtener informacion sobre Linkop">
+									<img src="<?php echo $url; ?>vistas/img/whatsapp(2).png" style="padding-bottom: 5px;"></a>
+							</li>
+							<?php
+							/* 
+							$social = ControladorPlantilla::ctrEstiloPlantilla();
+
+							$jsonRedesSociales = json_decode($social["redesSociales"], true);
+
+							foreach ($jsonRedesSociales as $key => $value) {
+
+								if ($value["activo"] == 1) {
+
+									echo '<li>
+						<a href="' . $value["url"] . '" target="_blank">
+							<i class="fa ' . $value["red"] . ' redSocial ' . $value["estilo"] . '" aria-hidden="true"></i>
+						</a>
+					</li>';
+								}
+							} */
+
+							?>
 
 
+
+
+						</ul>
+
+					</div> -->
+
+				</div>
+
+			</div>
+
+		</div>
 	</div>
 
-	
+
 </footer>
 
 
