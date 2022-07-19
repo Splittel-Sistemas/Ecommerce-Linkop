1f90f9cfa7a4e755a9343a0b8a099093d1977170
RMN <50149082+Ramon-Olea@users.noreply.github.com>
2022-07-19 11:52:17 -0500

    Update respuesta.php

--- frontend/vistas/modulos/Conekta/respuesta.php
+++ frontend/vistas/modulos/Conekta/respuesta.php
@@ -1,14 +1,42 @@
 <?php
 
 $body = @file_get_contents('php://input');
-$data = json_decode($body);
-http_response_code(200); // Return 200 OK 
+$json 				= json_decode($body);
+$json 				= $json->data->object;
+
+$invoiceid 			= $json->reference_id;
+$fee 				= $json->fee;
+$amount 			= $json->amount;
+$status				= $json->status;
+$transid 			= $json->id;
 
 if ($data->type == 'charge.paid'){
   $msg = "Tu pago ha sido comprobado.";
   mail("fulanito@conekta.com","Pago confirmado",$msg);
 }
-ini_set('display_errors', 1);
-ini_set("log_errors", 1);
-ini_set("error_log",  "conekta_error_log");
-ini_set($data->type ,  "conekta_error_log");
+
+// Validamos que el IPN sea de Banorte
+
+	// Guardar Log de webhook (comentar esto para no guardar logs)
+	$fp = fopen('oxxo_'.md5(uniqid()).".txt","wb");
+	fwrite($fp,$result);
+	fclose($fp);
+	
+	// Convertimos montos con decimales
+	$amount_2 			= substr($amount, 0, -2);
+	$decimals_2 		= substr($amount, strlen($amount_2), strlen($amount));
+	$amount				= $amount_2.'.'.$decimals_2;
+	
+	$amount_3 			= substr($fee, 0, -2);
+	$decimals_3 		= substr($fee, strlen($amount_3), strlen($fee));
+	$fee				= $amount_3.'.'.$decimals_3;
+	
+	$invoiceid 			= str_replace('factura_', '', $invoiceid);
+	
+	if($status=='paid'){$status=1;}else{$status=0;}
+	
+	
+	
+
+
+header("HTTP/1.0 200");
