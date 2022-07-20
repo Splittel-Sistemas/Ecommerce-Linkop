9ad71187b60be992f334fcc243aed1a3fafd0c18
RMN <50149082+Ramon-Olea@users.noreply.github.com>
2022-07-20 17:00:19 -0500

    Update grafico-ventas.php

--- frontend/backend/vistas/modulos/inicio/grafico-ventas.php
+++ frontend/backend/vistas/modulos/inicio/grafico-ventas.php
@@ -13,31 +13,44 @@ foreach ($ventas as $key => $value) {
 	/*=============================================
   	PORCENTAJES MÉTODOS DE PAGO PAYPAL
   	=============================================*/
-  	if($value["metodo"] == "paypal"){
+	if ($value["metodo"] == "paypal") {
 
-  		 $totalPaypal += $value["pago"];
+		$totalPaypal += $value["pago"];
 
-  		 $porcentajePaypal = $totalPaypal * 100 / $totalVentas["total"];
-  	}
+		$porcentajePaypal = $totalPaypal * 100 / $totalVentas["total"];
+	}
 
-  	/*=============================================
+	/*=============================================
   	PORCENTAJES MÉTODOS DE PAGO PAYU
   	=============================================*/
-  	if($value["metodo"] == "payu"){
+	/* if($value["metodo"] == "payu"){
 
   		 $totalPayu += $value["pago"];
 
   		 $porcentajePayu = $totalPayu * 100 / $totalVentas["total"];
-  	}
+  	} */
+	if ($value["metodo"] == "oxxo") {
+
+		$totaloxxo += $value["pago"];
+
+		$porcentajeoxxo = $totaloxxo * 100 / $totalVentas["total"];
+	}
+	if ($value["metodo"] == "open pay") {
+
+		$totalopen += $value["pago"];
+
+		$porcentajeopen = $totalopen * 100 / $totalVentas["total"];
+	}
+
 
 	/*=============================================
   	GRÁFICA EN LÍNEA
   	=============================================*/
-	
-	if($value["metodo"] != "gratis"){
+
+	if ($value["metodo"] != "gratis") {
 
 		#Capturamos sólo el año y el mes
-		$fecha = substr($value["fecha"],0,7);
+		$fecha = substr($value["fecha"], 0, 7);
 
 		#Capturamos las fechas en un array
 		array_push($arrayFechas, $fecha);
@@ -47,14 +60,10 @@ foreach ($ventas as $key => $value) {
 
 		#Sumamos los pagos que ocurrieron el mismo mes
 		foreach ($arrayFechaPago as $key => $value) {
-			
+
 			$sumaPagosMes[$key] += $value;
 		}
-
 	}
-
-
-
 }
 
 #Evitamos repetir fecha
@@ -74,14 +83,14 @@ GRÁFICO DE VENTAS
 
 		<i class="fa fa-th"></i>
 
-	    <h3 class="box-title">Gráfico de Ventas</h3>
+		<h3 class="box-title">Gráfico de Ventas</h3>
 
-	    <div class="box-tools pull-right">
-	      
-	      <button type="button" class="btn bg-teal btn-sm" data-widget="collapse"><i class="fa fa-minus"></i>
-	      </button>
+		<div class="box-tools pull-right">
 
-	    </div>
+			<button type="button" class="btn bg-teal btn-sm" data-widget="collapse"><i class="fa fa-minus"></i>
+			</button>
+
+		</div>
 
 	</div>
 	<!-- box-header -->
@@ -95,70 +104,81 @@ GRÁFICO DE VENTAS
 	<!-- box-body -->
 
 	<!-- box-footer -->
-  	<div class="box-footer no-border">
+	<div class="box-footer no-border">
 
-	  	<!-- row -->
-	    <div class="row">
-	    
-	      <div class="col-xs-6 text-center" style="border-right: 1px solid #f4f4f4">
-	    
-	        <input type="text" class="knob" data-readonly="true" value="<?php echo round($porcentajePaypal); ?>" data-width="60" data-height="60" data-fgColor="#39CCCC">
+		<!-- row -->
+		<div class="row">
 
-	        <div class="knob-label">Paypal</div>
-	      
-	      </div>
+			<div class="col-xs-4 text-center" style="border-right: 1px solid #f4f4f4">
 
-	      <div class="col-xs-6 text-center" style="border-right: 1px solid #f4f4f4">
-	        
-	        <input type="text" class="knob" data-readonly="true" value="<?php echo round($porcentajePayu); ?>" data-width="60" data-height="60" data-fgColor="#39CCCC">
+				<input type="text" class="knob" data-readonly="true" value="<?php echo round($porcentajePaypal); ?>" data-width="60" data-height="60" data-fgColor="#BC4031">
 
-	        <div class="knob-label">Payu</div>
-	      
-	      </div>
+				<div class="knob-label">Paypal</div>
 
-	    </div>
-	    <!-- row -->
+			</div>
 
-  	</div>
-  	<!-- box-footer -->
+			<!-- 	<div class="col-xs-4 text-center" style="border-right: 1px solid #f4f4f4">
+
+				<input type="text" class="knob" data-readonly="true" value="<?php echo round($porcentajePayu); ?>" data-width="60" data-height="60" data-fgColor="#BC4031">
+
+				<div class="knob-label">Payu</div>
+
+			</div> -->
+			<div class="col-xs-4 text-center" style="border-right: 1px solid #f4f4f4">
+
+				<input type="text" class="knob" data-readonly="true" value="<?php echo round($porcentajeoxxo); ?>" data-width="60" data-height="60" data-fgColor="#BC4031">
+
+				<div class="knob-label">oxxo</div>
+
+			</div>
+			<div class="col-xs-4 text-center" style="border-right: 1px solid #f4f4f4">
+
+				<input type="text" class="knob" data-readonly="true" value="<?php echo round($porcentajeopen); ?>" data-width="60" data-height="60" data-fgColor="#BC4031">
+
+				<div class="knob-label">open pay</div>
+
+			</div>
+
+		</div>
+		<!-- row -->
+
+	</div>
+	<!-- box-footer -->
 
 </div>
 <!-- solid sales graph -->
 
 <script>
-	
-var line = new Morris.Line({
-    element          : 'line-chart',
-    resize           : true,
-    data             : [
+	var line = new Morris.Line({
+		element: 'line-chart',
+		resize: true,
+		data: [
 
-    <?php
+			<?php
 
-    	foreach ($noRepetirFechas as $value) {
-    	
-    	echo "{ y: '".$value."', ventas: ".$sumaPagosMes[$value]." },";
-    		
-    	}
+			foreach ($noRepetirFechas as $value) {
 
-      echo "{ y: '".$value."', ventas: ".$sumaPagosMes[$value]." }";
+				echo "{ y: '" . $value . "', ventas: " . $sumaPagosMes[$value] . " },";
+			}
 
-    ?>
+			echo "{ y: '" . $value . "', ventas: " . $sumaPagosMes[$value] . " }";
 
-    ],
-    xkey             : 'y',
-    ykeys            : ['ventas'],
-    labels           : ['Ventas'],
-    lineColors       : ['#efefef'],
-    lineWidth        : 2,
-    hideHover        : 'auto',
-    gridTextColor    : '#fff',
-    gridStrokeWidth  : 0.4,
-    pointSize        : 4,
-    pointStrokeColors: ['#efefef'],
-    gridLineColor    : '#efefef',
-    gridTextFamily   : 'Open Sans',
-    preUnits		 : '$',
-    gridTextSize     : 10
-  });
-	
+			?>
+
+		],
+		xkey: 'y',
+		ykeys: ['ventas'],
+		labels: ['Ventas'],
+		lineColors: ['#efefef'],
+		lineWidth: 2,
+		hideHover: 'auto',
+		gridTextColor: '#fff',
+		gridStrokeWidth: 0.4,
+		pointSize: 4,
+		pointStrokeColors: ['#efefef'],
+		gridLineColor: '#efefef',
+		gridTextFamily: 'Open Sans',
+		preUnits: '$',
+		gridTextSize: 10
+	});
 </script>
