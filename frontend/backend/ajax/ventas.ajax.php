eb53137075b0d0a24f9c3af8c6ca18d7d4e818ee
RMN <50149082+Ramon-Olea@users.noreply.github.com>
2022-07-20 15:11:15 -0500

    envio de mensaje para cambio de status

--- frontend/backend/ajax/ventas.ajax.php
+++ frontend/backend/ajax/ventas.ajax.php
@@ -3,7 +3,8 @@
 require_once "../controladores/ventas.controlador.php";
 require_once "../modelos/ventas.modelo.php";
 
-
+/* use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception; */
 class AjaxVentas{
 
 	/*=============================================
@@ -37,3 +38,59 @@ if(isset($_POST["idVenta"])){
 	$envioVenta -> ajaxEnvioVenta();
 
 }
+/* date_default_timezone_set("America/Bogota");
+
+
+$mail = new PHPMailer;
+
+$mail->CharSet = 'UTF-8';
+
+$mail->isMail();
+
+$mail->setFrom('notificaciones-splitnet@splittel.com', 'Linkop');
+
+
+
+$mail->Subject = "Por favor verifique su dirección de correo electrónico";
+
+$mail->addAddress($_POST["regEmail"]);
+
+$mail->msgHTML('<div style="width:100%; background:#eee; position:relative; font-family:sans-serif; padding-bottom:40px">
+	
+	<center>
+		
+		<img style="padding:20px; width:10%" src="">
+
+	</center>
+
+	<div style="position:relative; margin:auto; width:600px; background:white; padding:20px">
+	
+		<center>
+		
+		<img style="padding:20px; width:15%" src="">
+
+		<h3 style="font-weight:100; color:#999">VERIFIQUE SU DIRECCIÓN DE CORREO ELECTRÓNICO</h3>
+
+		<hr style="border:1px solid #ccc; width:80%">
+
+		<h4 style="font-weight:100; color:#999; padding:0 20px">Para comenzar a usar su cuenta de Tienda Virtual, debe confirmar su dirección de correo electrónico</h4>
+
+		<a href="'.$url.'verificar/'.$encriptarEmail.'" target="_blank" style="text-decoration:none">
+
+		<div style="line-height:60px; background:#0aa; width:60%; color:white">Verifique su dirección de correo electrónico</div>
+
+		</a>
+
+		<br>
+
+		<hr style="border:1px solid #ccc; width:80%">
+
+		<h5 style="font-weight:100; color:#999">Si no se inscribió en esta cuenta, puede ignorar este correo electrónico y la cuenta se eliminará.</h5>
+
+		</center>
+
+	</div>
+
+</div>');
+
+$envio = $mail->Send(); */
