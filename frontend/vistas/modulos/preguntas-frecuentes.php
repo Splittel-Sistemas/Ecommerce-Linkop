b7ada36466e74d46250dd7b0ec9a52601faff302
RMN <50149082+Ramon-Olea@users.noreply.github.com>
2022-07-22 18:01:46 -0500

    Update preguntas-frecuentes.php

--- frontend/vistas/modulos/preguntas-frecuentes.php
+++ frontend/vistas/modulos/preguntas-frecuentes.php
@@ -8,16 +8,16 @@ BANNER
 
 
 
-<!------ Include the above in your HEAD tag ---------->
+<!-----Include the above in your HEAD tag ---------->
 
 
 
-<!-- <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<!--<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script> -->
-<!------ Include the above in your HEAD tag ---------->
+<!-----Include the above in your HEAD tag ---------->
 
-<!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet"> 
+<!--<link href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet"> 
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">  -->
 
 <style>
@@ -86,7 +86,7 @@ BANNER
 </style>
 
 <section id="blog">
-	<div class="container">
+	<div class="container text-center">
 		<div class="row">
 			<div class="col-lg-12 text-center">
 				<h2 style="color:#000000">
@@ -96,33 +96,99 @@ BANNER
 				</h2>
 			</div>
 		</div>
+		<div class="accordion" id="accordionExample">
 
+			<div class="card">
+				<div class="card-header" id="headingOne">
+					<h2 class="mb-0">
+						<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+							¿Cómo puedo comprar?
+						</button>
+					</h2>
+				</div>
+				<div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+					<div class="card-body">
+						En Link Op es muy fácil comprar, puedes crear una cuenta directamente en nuestra plataforma o comprar como invitado. Queremos que cada segundo que pases en nuestra plataforma sea una experiencia sencilla y agradable.
+						Para registrarte sigue estos pasos:<br>
+						 Visita la página de link op<br>
+						Accede al botón de login con crear cuenta<br>
+						 Coloca tu nombre, apellido y dirección de correo electrónico<br>
+						Listo ahora ya eres parte de nuestra comunidad.<br>
+						</div>
+				</div>
+			</div>
+			<div class="card">
+				<div class="card-header" id="heading4">
+					<h2 class="mb-0">
+						<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse4" aria-expanded="false" aria-controls="collapse4">
+						¿Cómo compro como invitado?
+
+						</button>
+					</h2>
+				</div>
+				<div id="collapse4" class="collapse" aria-labelledby="heading4" data-parent="#accordionExample">
+					<div class="card-body">
+						
+						1. Selecciona tu producto de interés <br>
+						2. Da clic en comprar ahora<br>
+						3. Coloca tu nombre completo<br>
+						4. Coloca tu correo electrónico<br>
+						5. Acepta nuestras políticas de términos y condiciones </div>
+				</div>
+			</div>
+			<div class="card">
+				<div class="card-header" id="headingTwo">
+					<h2 class="mb-0">
+						<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+							¿Qué pasa si mi producto no llega?
+						</button>
+					</h2>
+				</div>
+				<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+					<div class="card-body">
+						Si tu producto no llegó a tiempo, ponte en contacto con nosotros a través de nuestro CHAT o envíanos un mensaje a reclamos@linkop.mx.
+						
+
+					</div>
+				</div>
+			</div>
+			<div class="card">
+				<div class="card-header" id="heading5">
+					<h2 class="mb-0">
+						<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse5" aria-expanded="false" aria-controls="collapse5">
+						¿Qué pasa si mi producto llega dañado?						</button>
+					</h2>
+				</div>
+				<div id="collapse5" class="collapse" aria-labelledby="heading5" data-parent="#accordionExample">
+					<div class="card-body">
+					Nuestra política de devoluciones te ampara, si el producto te llegó dañado físico y funcionalmente, tienes 30 días para solicitar tu nuevo producto, después del periodo asignado pierdes la garantía.
+					</div>
+				</div>
+			</div>
+			<div class="card">
+				<div class="card-header" id="headingThree">
+					<h2 class="mb-0">
+						<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+							¿Cuánto cuesta mi envío si compro menos de $500 pesos?
+						</button>
+					</h2>
+				</div>
+				<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+					<div class="card-body">
+						Nuestro envío es estándar para toda la república, 300 pesos para el norte del país, 250 para el centro de México y 450 para el sur de México.
+					</div>
+				</div>
+			</div>
+		</div>
 		<div class="row">
 			<div id="slider" class="carousel slide" data-ride="carousel">
-				<!-- <ol class="carousel-indicators">
+				<!--<ol class="carousel-indicators">
 					<li data-target="#slider" data-slide-to="1" class="active"></li>
 					<li data-target="#slider" data-slide-to="0"></li>
 				</ol> -->
 
-				¿Cómo puedo comprar?
-				- En Link Op es muy fácil comprar, puedes crear una cuenta directamente en nuestra plataforma o comprar como invitado. Queremos que cada segundo que pases en nuestra plataforma sea una experiencia sencilla y agradable.
-				- Para registrarte sigue estos pasos:
-				o Visita la página de link op
-				o Accede al botón de login con crear cuenta
-				o Coloca tu nombre, apellido y dirección de correo electrónico
-				o Listo ahora ya eres parte de nuestra comunidad.
-				¿Cómo compro como invitado?
-				- 1. Selecciona tu producto de interés
-				- 2. Da clic en comprar ahora
-				- 3. Coloca tu nombre completo
-				- 4. Coloca tu correo electrónico
-				- 5. Acepta nuestras políticas de términos y condiciones
-				¿Qué pasa si mi producto no llega?
-				- Si tu producto no llegó a tiempo, ponte en contacto con nosotros a través de nuestro CHAT o envíanos un mensaje a reclamos@linkop.mx.
-				¿Cuánto cuesta mi envío si compro menos de $500 pesos?
-				- Nuestro envío es estándar para toda la república, 300 pesos para el norte del país, 250 para el centro de México y 450 para el sur de México.
-				¿Qué pasa si mi producto llega dañado?
-				- Nuestra política de devoluciones te ampara, si el producto te llegó dañado físico y funcionalmente, tienes 30 días para solicitar tu nuevo producto, después del periodo asignado pierdes la garantía.
+
+
 
 			</div>
 
