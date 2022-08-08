667c987ea8409194069b7d99c18719e6a516defa
RMN <50149082+Ramon-Olea@users.noreply.github.com>
2022-08-08 13:19:10 -0500

    Update contacto.php

--- frontend/vistas/modulos/contacto.php
+++ frontend/vistas/modulos/contacto.php
@@ -7,8 +7,9 @@
         </div>
          <div class="col-md-6 ">
           <div class="card mb-30">
-             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3734.214127360639!2d-100.40539448532522!3d20.620127086221146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d35ae461d52039%3A0x3e02826339c370c!2sFibremex+S.A.+de+C.V.!5e0!3m2!1ses!2smx!4v1510775709351" style="border:0" allowfullscreen="" height="400px" width="550px" frameborder="0"></iframe>
-            <div class="card-body">
+<!--              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3734.214127360639!2d-100.40539448532522!3d20.620127086221146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d35ae461d52039%3A0x3e02826339c370c!2sFibremex+S.A.+de+C.V.!5e0!3m2!1ses!2smx!4v1510775709351" style="border:0" allowfullscreen="" height="400px" width="550px" frameborder="0"></iframe>
+ -->        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3735.8177908486214!2d-100.27327618528209!3d20.554634809170842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d343e44cd2c1db%3A0x448932fde9382fe0!2sGrupo%20Splittel!5e0!3m2!1ses-419!2smx!4v1659982657581!5m2!1ses-419!2smx"  height="400px" width="550px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+     <div class="card-body">
               <ul class="list-icon">
                 <li><i class="icon-map-pin text-muted"></i>Parque Tecnológico Innovación Querétaro, Lateral de la carretera Estatal 431, km.2+200, Int.28, C.P.76246</li>
               </ul>
@@ -26,11 +27,9 @@ Estamos a sus órdenes.</h5>
               <i class="icon-mail text-muted"></i>
               <a class="navi-link" href="mailto:info@linkop.com.mx">info@linkop.com.mx</a>
             </li>
-            <li> <i class="icon-clock text-muted"></i>Lunes-Viernes: 8:00 am - 8:00 pm<br>A partir del 19 de febrero tendremos horario sabatino de 9:00 am - 5:00 pm</li>
+            <li> <i class="icon-clock text-muted"></i>Lunes-Viernes: 8:00 am - 8:00 pm </li>
             <li> <i class="icon-globe text-muted"></i>https://linkop.com.mx/</li>
-         <!--   <li> <i class="icon-phone text-muted"></i>+1 (080) 44 357 260</li>
-            <li> <i class="icon-clock text-muted"></i>1 - 2 business days</li>
-         -->
+        
           </ul>
         </div>
       </div>
