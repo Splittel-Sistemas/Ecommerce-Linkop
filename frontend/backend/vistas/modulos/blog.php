8ed65ad6342e7208fc6cc7436fc58363d1087068
Ramon Olea <50149082+Ramon-Olea@users.noreply.github.com>
2022-09-23 11:52:59 -0500

    subir imagenes locales editor blog

--- frontend/backend/vistas/modulos/blog.php
+++ frontend/backend/vistas/modulos/blog.php
@@ -17,6 +17,49 @@
   </section>
   <script src="vistas/plugins/tinymce5.7/tinymce.min.js" referrerpolicy="origin"></script>
   <script>
+     function example_image_upload_handler (blobInfo, success, failure, progress) {
+  var xhr, formData;
+
+  xhr = new XMLHttpRequest();
+  xhr.withCredentials = false;
+  xhr.open('POST', 'vistas/modulos/postAcceptor.php');
+
+  xhr.upload.onprogress = function (e) {
+    progress(e.loaded / e.total * 100);
+  };
+
+  xhr.onload = function() {
+    var json;
+
+    if (xhr.status === 403) {
+      failure('HTTP Error: ' + xhr.status, { remove: true });
+      return;
+    }
+
+    if (xhr.status < 200 || xhr.status >= 300) {
+      failure('HTTP Error: ' + xhr.status);
+      return;
+    }
+
+    json = JSON.parse(xhr.responseText);
+
+    if (!json || typeof json.location != 'string') {
+      failure('Invalid JSON: ' + xhr.responseText);
+      return;
+    }
+
+    success(json.location);
+  };
+
+  xhr.onerror = function () {
+    failure('Image upload failed due to a XHR Transport error. Code: ' + xhr.status);
+  };
+
+  formData = new FormData();
+  formData.append('file', blobInfo.blob(), blobInfo.filename());
+
+  xhr.send(formData);
+};
     tinymce.init({
       content_css: "/mycontent.css",
       language: 'es_MX',
@@ -46,6 +89,8 @@
         strikethrough: false,
         underline: false
       },
+      images_upload_handler: example_image_upload_handler,
+
       tinycomments_mode: 'embedded',
       tinycomments_author: 'Supervisor',
       table_toolbar: "tableprops cellprops tabledelete | tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol",
